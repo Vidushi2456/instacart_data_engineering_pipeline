823662c1e3ed561b8a48a320e46b4b9eaa745baf
Vidushi Chaudhary <vidushichaudhary99@gmail.com>
2026-04-03 20:32:52 +0530

    Added multi-table injestion pipeline

--- scripts/injest.py
+++ scripts/injest.py
@@ -1,10 +1,30 @@
 import pandas as pd
 
-# load sample dataset (keep small)
-orders = pd.read_csv('../data/orders.csv')
+DATA_PATH = "C:/Users/DELL/OneDrive/Desktop/instacart_data_engineering_pipeline/data/"
 
-print("Preview:")
-print(orders.head())
+def load_data():
+    print("Starting data ingestion...")
 
-print("\nInfo:")
-print(orders.info())
+    orders = pd.read_csv(DATA_PATH + "orders_sample.csv")
+    order_products = pd.read_csv(DATA_PATH + "order_products__prior_sample.csv")
+    products = pd.read_csv(DATA_PATH + "products_sample.csv")
+    aisles = pd.read_csv(DATA_PATH + "aisles_sample.csv")
+    departments = pd.read_csv(DATA_PATH + "departments_sample.csv")
+
+    print("All datasets loaded successfully!\n")
+
+    return {
+        "orders": orders,
+        "order_products": order_products,
+        "products": products,
+        "aisles": aisles,
+        "departments": departments
+    }
+
+
+if __name__ == "__main__":
+    data = load_data()
+
+    # preview one dataset
+    print("Orders Preview:")
+    print(data["orders"].head())
