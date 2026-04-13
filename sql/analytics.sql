c2420f3e083f7c6b9af7885b80b4cac90bc28b6b
Vidushi Chaudhary <vidushichaudhary99@gmail.com>
2026-04-13 21:33:10 +0530

    Added analytics layer, fixed joins and sampling, integrated Snowflake setup

--- sql/analytics.sql
+++
@@ -1,8 +0,0 @@
---top selling products
-df.groupby("product_name")["order_id"].count().sort_values(ascending=False).head(10);
---most reordered products
-df.groupby("product_name")["reordered"].sum().sort_values(ascending=False).head(10);
---peak order hour
-df.groupby("order_hour_of_day")["order_id"].count().sort_values(ascending=False);
---orders per user
-df.groupby("order_hour_of_day")["order_id"].count().sort_values(ascending=False);
